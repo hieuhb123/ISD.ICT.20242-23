abf259dc4e6d80465b03a7db12f23f7cc9dce8fb
Truong Giang <163492019+TruoYang@users.noreply.github.com>
2025-05-18 12:44:30 +0700

    comment cohesion & SRP

--- src/main/java/com/media_shop/service/OrderService.java
+++ src/main/java/com/media_shop/service/OrderService.java
@@ -5,6 +5,9 @@ import com.media_shop.exception.OrderCannotBeCancelledException;
 import com.media_shop.exception.OrderNotFoundException;
 import com.media_shop.repository.OrderRepository;
 
+// High cohesion - only order-relate to business logic
+// Does not violate SRP - class focuses on cancelOrder logic
+
 public class OrderService {
     private final OrderRepository repository;
 
