818bf9683b5fdec9621c61dbd1a31f46115ceb4f
hieuhb123 <sieukhoe654321@gmail.com>
2025-07-04 22:43:26 +0700

    delete book, cd, dvd repository

--- src/server/src/main/java/com/media_shop/repository/product/DVDRepository.java
+++
@@ -1,9 +0,0 @@
-package com.media_shop.repository.product;
-
-import com.media_shop.entity.product.DVD;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DVDRepository extends MongoRepository<DVD, String> {
-}
